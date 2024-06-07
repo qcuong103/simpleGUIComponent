6b1422a898fa7ca03d05b13dda0b05b521ff648f
qcuong_103 <quoccuong100300@gmail.com>
2024-06-07 22:34:59 +0700

    final

---
+++ app/src/main/java/com/cuong_103/bai5cacthanhphangiaodiencoban/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cuong_103.bai5cacthanhphangiaodiencoban;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
